8f45345305a1282ab450e7ba63cb3037527eb9b7
WebD01 <liangu@protonmail.com>
2019-01-24 10:12:15 -0500

    Logs when user get kicked

--- src/commands/moderation/Kick.ts
+++ src/commands/moderation/Kick.ts
@@ -1,10 +1,11 @@
 import { GuildMember, Message } from 'discord.js';
+import { log } from '../../API/logger';
 import { Bot } from '../../Bot';
 import { confirmationEmbed, errorEmbed } from '../../embed';
 import { isAdmin } from '../../utils';
 
 export default async function run(
-    { config }: Bot,
+    { config: { color } }: Bot,
     message: Message,
     [, ...rest]: string[]
 ): Promise<any> {
@@ -26,11 +27,16 @@ export default async function run(
             embed: confirmationEmbed(
                 'You have been kicked',
                 reason,
-                config.color,
+                color,
                 author
             ),
         });
         await toKick.kick(reason);
+        log('Member kicked', color, guild, {
+            Member: `<@${toKick.id}>`,
+            Moderator: `<@${member.id}>`,
+            Reason: reason,
+        });
         return channel.send(
             `<@${toKick.id}> has been kicked for: **${reason}**`
         );
