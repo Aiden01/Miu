e01db62a8ecd2bcba2cc5afbb3c9a98e38cf105c
WebD01 <liangu@protonmail.com>
2019-01-26 15:45:20 -0500

    fix typo

--- src/registerSimpleCommands.ts
+++ src/registerSimpleCommands.ts
@@ -2,6 +2,6 @@ export default [
     {
         name: 'support',
         response:
-            'If you are having an issue with the bot, you can submit an issue using ``~~report``.\n You can also join our official Discord server: https://discord.gg/8WmEVV3',
+            'If you are having a problem with the bot, you can submit an issue using ``~~report``.\n You can also join our official Discord server: https://discord.gg/8WmEVV3',
     },
 ];
